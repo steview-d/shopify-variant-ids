34eed2bbb423b5b08c85a367be5b5a5359aed9e2
Dave Laffan <dave.laffan@outlook.com>
2020-10-10 22:56:20 +0100

    Store ID's in a list

--- david.py
+++ david.py
@@ -1,12 +1,14 @@
 import json, re, requests
 from bs4 import BeautifulSoup
 
+id_list = []
+
 
 def get_variant_id():
     url = input("Please enter product full URL (or 'x' to go back): ")
 
     if url == 'x':
-        return
+        user_options()
 
     try:
         r = requests.get(url)
@@ -23,9 +25,31 @@ def get_variant_id():
     print('-' * len(name))
     print(name)
     print('-' * len(name))
-    for i in json_data['product']['variants']:
-        print(f"{i['public_title']} - {i['id']}")
+    for idx, i in enumerate(json_data['product']['variants']):
+        print(f"[{idx + 1}] - {i['public_title']} - {i['id']}")
 
+    num_ids = len(json_data['product']['variants'])
+    variant_nums = []
+    for n in range(1, num_ids + 1):
+         variant_nums.append(str(n))
+
+
+    option = input("Select variant ID line number to copy to list, or go [b]ack. ")
+
+    if option == 'b':
+        user_options()
+    elif option in variant_nums:
+       id_list.append(json_data['product']['variants'][int(option) - 1]['id'])
+    else:
+        pass
+
+
+def list_ids():
+    print("Variant IDs")
+    print("-----------")
+    for i in id_list:
+        print(i)
+    print("-----------")
 
 def print_to_screen(text_list):
     for text in text_list:
@@ -36,7 +60,7 @@ def title():
     print_to_screen(title_data)
 
 def user_options():
-    options_list = [' ', '1 - Get Variant ID', '2 - List All Stored IDs', '3 - Exit']
+    options_list = [' ', '1 - Get Variant ID', '2 - List All Stored IDs', '3 - Clear ID List', '4 - Exit']
     print_to_screen(options_list)
     print_to_screen(' ')
 
@@ -44,16 +68,18 @@ def user_options():
     if option == '1':
         get_variant_id()
     elif option == '2':
-        pass
+        list_ids()
     elif option == '3':
+        id_list.clear()
+        print_to_screen(['ID list has been cleared.'])
+    elif option == '4':
+        list_ids()
         print_to_screen(['Exiting now...'])
         quit()
     else:
         user_options()
 
 
-
 title()
-
 while True:
     user_options()
