076994971e856f7d21fed96be368e21635135a50
Dave Laffan <dave.laffan@outlook.com>
2020-10-10 18:48:45 +0100

    Pull id data and store as a dict

--- david.py
+++ david.py
@@ -1,8 +1,17 @@
 # Deadly Accurate Variant Id Detector
 
-import requests
+import json, re, requests
 from bs4 import BeautifulSoup
 
 # temp vars
 url = 'https://e-liquids.com/collections/50-50-vape-juices/products/six-licks-50-50-melon-on-my-mind-10ml'
 
+# var meta = {"product":
+
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+result = str(soup.find('script', text=re.compile('var meta = {"product":')))
+foo = result.split('var meta = ')[1].split('for (var attr in')[0]
+json_data = json.loads(foo[:-2])
+print(type(json_data))
+print(json_data)
