17d5be823e7f678722da0fcd262845d384f66d74
Dave Laffan <dave.laffan@outlook.com>
2020-10-11 11:41:55 +0100

    Add option to export csv to screen
    ... and to select multiple variants at once.

--- david.py
+++ david.py
@@ -1,4 +1,6 @@
-# issue with recursions - if keep entering wrong url, when correct one entered, bugs out
+# add option to email results
+# make script standalone
+
 
 import json, re, requests
 from bs4 import BeautifulSoup
@@ -18,28 +20,54 @@ def get_variant_id():
         print_to_screen(['Invalid URL'])
         return
 
+    # Scrape data from product page
     soup = BeautifulSoup(r.text, 'html.parser')
     result = str(soup.find('script', text=re.compile('var meta = {"product":')))
     data = result.split('var meta = ')[1].split(',"page":')[0]
     json_data = json.loads(data + '}')
 
+    # Display product name and variants
     name = json_data['product']['variants'][0]['name'].split(' - ')[0]
     print('-' * len(name))
     print(name)
     print('-' * len(name))
     for idx, i in enumerate(json_data['product']['variants']):
         print(f"[{idx + 1}] - {i['public_title']} - {i['id']}")
+    print('-' * len(name))
 
+    # Store variant name & IDs
     num_ids = len(json_data['product']['variants'])
     variant_nums = []
     for n in range(1, num_ids + 1):
          variant_nums.append(str(n))
 
-
-    option = input("Select variant ID line number to copy to list, or go [b]ack. ")
+    # User options
+    print_to_screen(['Select variant ID line number to copy to list, or'])
+    print_to_screen(['Select multiple variants like so... [1,2,3] or'])
+    option = input("Go [b]ack. ")
 
     if option == 'b':
         user_options()
+    elif ',' in option:
+        try:
+            choices = option.split(',')
+        except:
+            print_to_screen({'Input not recognised...'})
+            return
+        else:
+            # Where user ends input with a ',' remove last list item
+            if choices[len(choices)-1] == '':
+                choices.pop()
+
+            # where multiple IDs selected, add them all to the list
+            for c in choices:
+                try:
+                    id_list.append(json_data['product']['variants'][int(c) - 1]['id'])
+                except:
+                    print_to_screen({'Input not recognised...'})
+                    return
+
+    # If only a single variant selected, add it to the list
     elif option in variant_nums:
        id_list.append(json_data['product']['variants'][int(option) - 1]['id'])
     else:
@@ -47,22 +75,50 @@ def get_variant_id():
 
 
 def list_ids():
+    # List all stored variant IDs
     print("Variant IDs")
     print("-----------")
     for i in id_list:
         print(i)
     print("-----------")
 
+
+def export_id_list():
+    if not id_list:
+        print_to_screen(['There are no IDs to export...'])
+        return
+
+    export_options = ['[1] - Print to screen as .csv', '[2] - Email as .csv']
+
+    #make csv list...
+    ids_csv = ''
+    for i in id_list:
+        ids_csv += f"{i},"
+
+    ids_csv = ids_csv[:-1]
+
+
+    print_to_screen(export_options)
+    option = input('Choose an Export option: ')
+    if option == '1':
+        print('----------------')
+        print_to_screen([ids_csv])
+        print('----------------')
+    elif option == '2':
+        # email export here...
+        pass
+    return
+
+
 def print_to_screen(text_list):
+    # Simple function for printing list items
     for text in text_list:
         print(text)
 
-def title():
-    title_data = ['DAVID -v1', '---------', 'Deadly Accurate Variant Id Detector' ]
-    print_to_screen(title_data)
 
 def user_options():
-    options_list = [' ', '1 - Get Variant ID', '2 - List All Stored IDs', '3 - Clear ID List', '4 - Exit']
+    # Initial options
+    options_list = [' ', '1 - Get Variant ID', '2 - List All Stored IDs', '3 - Export ID List', '4 - Clear ID List', '5 - Exit']
     print_to_screen(options_list)
     print_to_screen(' ')
 
@@ -72,16 +128,18 @@ def user_options():
     elif option == '2':
         list_ids()
     elif option == '3':
+        export_id_list()
+    elif option == '4':
         id_list.clear()
         print_to_screen(['ID list has been cleared.'])
-    elif option == '4':
+    elif option == '5':
         list_ids()
         print_to_screen(['Exiting now...'])
         quit()
     else:
-        user_options()
+       return
 
 
-title()
+print_to_screen([ '---------', 'DAVID -v1', '---------', 'Deadly Accurate Variant Id Detector' ])
 while True:
     user_options()
